cf791048850916259786784691ebde8fa5c2e50c
Evan Sprecher <EvanSprecher7@gmail.com>
2023-07-24 19:05:11 -0500

    ERS: Adding postgreSQL files: .sql for table setup and instructions README.

---
+++ Resources/PostgreSQL_setup_README.txt
@@ -0,0 +1,8 @@
+## SQL Database Setup and Data Import 
+
+1) in pgAdmin, create new Database titled "Project_3"
+2) Right-click the Project_3 Database and select "Query Tool"
+3) Above the Query tool text input window, click "Open File"
+4) Open the Project_3_tables_schema.sql file here. Execute the query here to populate tables.
+5) Manually import the CSV files from the Data folder by right-clicking each table in pgAdmin and selecting "Import/Export Data..."
+6) Confirm all data imported by running SELECT * statements in pgAdmin query window.
---
+++ Resources/Project_3_tables_schema.sql
@@ -0,0 +1,70 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- ---------------------------------------------------------------------
+-- Project 3 tables
+
+CREATE TABLE "county_avg_temperature" (
+    "County" VARCHAR   NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "State Abbr" VARCHAR   NOT NULL,
+    "Avg Temperature (F)" FLOAT   NOT NULL,
+    "Year" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "county_avg_precipitation" (
+    "County" VARCHAR  NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "State Abbreviation" VARCHAR   NOT NULL,
+    "Precipitation (in)" FLOAT   NOT NULL,
+    "Year" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "WestNile-Case-Counts-by-County" (
+    "2001" VARCHAR,
+    "2002" VARCHAR,
+    "2003" VARCHAR,
+    "2004" VARCHAR,
+    "2005" VARCHAR,
+    "2006" VARCHAR,
+    "2007" VARCHAR,
+    "2008" VARCHAR,
+    "2009" VARCHAR,
+    "2010" VARCHAR,
+    "2011" VARCHAR,
+    "2012" VARCHAR,
+    "2013" VARCHAR,
+    "2014" VARCHAR,
+    "2015" VARCHAR,
+    "2016" VARCHAR,
+    "2017" VARCHAR,
+    "2018" VARCHAR,
+    "2019" VARCHAR,
+    "2020" VARCHAR,
+    "county" VARCHAR
+);
+
+CREATE TABLE "LD-Case_Counts-by-County" (
+    "2001" VARCHAR,
+    "2002" VARCHAR,
+    "2003" VARCHAR,
+    "2004" VARCHAR,
+    "2005" VARCHAR,
+    "2006" VARCHAR,
+    "2007" VARCHAR,
+    "2008" VARCHAR,
+    "2009" VARCHAR,
+    "2010" VARCHAR,
+    "2011" VARCHAR,
+    "2012" VARCHAR,
+    "2013" VARCHAR,
+    "2014" VARCHAR,
+    "2015" VARCHAR,
+    "2016" VARCHAR,
+    "2017" VARCHAR,
+    "2018" VARCHAR,
+    "2019" VARCHAR,
+    "2020" VARCHAR,
+    "county" VARCHAR
+);
+
